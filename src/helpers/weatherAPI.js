5a57a0b9ee9175540505960fa011a9cf951593f8
analaurafr <laurafernandes@outlook.com>
2023-07-05 02:56:09 -0300

    iniciando projeto

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,16 @@
-// Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN;
+const API = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=`;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  const response = await fetch(API + term);
+  const data = await response.json();
+
+  if (data.length === 0) {
+    window.alert('Nenhuma cidade encontrada');
+    return [];
+  }
+
+  return data;
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
