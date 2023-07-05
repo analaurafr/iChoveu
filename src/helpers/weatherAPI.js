36d0ba10f7b2315357c45ab5afbd6c8367da3b04
analaurafr <laurafernandes@outlook.com>
2023-07-05 17:12:18 -0300

    github

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,8 +1,7 @@
 const TOKEN = import.meta.env.VITE_TOKEN;
-const API = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=`;
 
 export const searchCities = async (term) => {
-  const response = await fetch(API + term);
+  const response = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`);
   const data = await response.json();
 
   if (data.length === 0) {
@@ -13,5 +12,14 @@ export const searchCities = async (term) => {
   return data;
 };
 
-export const getWeatherByCity = (/* cityURL */) => {
+export const getWeatherByCity = async (cityURL) => {
+  try {
+    const response = await (await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${cityURL}`)).json();
+
+    const { temp_c: temp, condition, current: { icon } } = response;
+
+    return { temp, condition: condition.text, icon };
+  } catch (error) {
+    window.alert('Error');
+  }
 };
